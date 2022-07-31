1788c285ef0da7165ddfb94b58f295d9ee3c3bdb
Luka Benedetti <benedetiluka@gmail.com>
2022-07-31 14:18:55 +0200

    new com

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import teslaData from "./data/tesla-sites.json";
 
 function App() {
+  let [country, setCountry] = useState("Italy");
+  const filteredStations = teslaData.filter(
+    (tsla) => tsla.address.country.toLowerCase() === country.toLowerCase()
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <>
+      <div className="tesla">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          SEARCH TESLA CHARGES BY <strong>COUNTRY</strong>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </div>
+
+      <input
+        onChange={(e) => setCountry(e.target.value)}
+        type="text"
+        placeholder={country}
+        value={country}
+      />
+
+      <MapContainer
+        center={[41.871941, 12.56738]}
+        zoom={6}
+        scrollWheelZoom={true}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+
+        {filteredStations.map((tsla) => {
+          if (tsla.status === "OPEN") {
+            var mystyle = {
+              color: "green",
+            };
+          } else {
+            var mystyle = {
+              color: "red",
+            };
+          }
+          return (
+            <Marker
+              key={tsla.id}
+              position={[tsla.gps.latitude, tsla.gps.longitude]}
+            >
+              <Popup position={[tsla.gps.latitude, tsla.gps.longitude]}>
+                <div>
+                  <h2>{tsla.name}</h2>
+                  <p style={mystyle}>{tsla.status}</p>
+                  <p>{tsla.address.region}</p>
+                </div>
+              </Popup>
+            </Marker>
+          );
+        })}
+      </MapContainer>
+    </>
   );
 }
 
